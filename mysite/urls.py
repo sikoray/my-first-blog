b719966a700affddc8db0d9cc06dd5a64b333a5b
sikoray <sikora.y@nton.info>
2016-03-04 04:04:44 +0200

    add image

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,19 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
-]
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+    {'document_root':'media'}),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+	urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
